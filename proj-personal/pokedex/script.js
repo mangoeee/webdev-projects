4411904e34e259b49ac55105d1280567010d409f
“Emery <emerygabriel.m@gmail.com>
2023-04-05 15:32:00 -0400

    connected API to name and sprite

--- proj-personal/pokedex/script.js
+++ proj-personal/pokedex/script.js
@@ -1,26 +1,36 @@
-// const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
-const searchBar = document.querySelector('input[type="search"]');
+let searchBar = document.querySelector('input[type="search"]');
 
 // Change sprite on input using pokemondb, not pokeAPI
 //
-searchBar.addEventListener('search', async () => {
+searchBar.addEventListener('search', async (input) => {
     input = searchBar.value;
-    spriteURL = `https://img.pokemondb.net/sprites/scarlet-violet/normal/${input.toLowerCase()}.png`;
     
-    try {
-        document.getElementById("sprite").src = spriteURL;
-    } catch(error) {
-        console.error(error)
-        document.getElementById('name').innerHTML = `Pokémon`;
+    // Display sprite from pokemondb
+    //
+    let spriteURL = `https://img.pokemondb.net/sprites/scarlet-violet/normal/${input.toLowerCase()}.png`;
+    document.getElementById("sprite").src = spriteURL;
+    // Fetch data from PokeAPI
+    //
+    const pokeAPI = `https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`;
+    const response = await fetch(pokeAPI);
+    // Catch and display error
+    //
+    if(response.status == 404 || response.statusText == 'Not Found'){
+        document.getElementById('error').classList.add('show')
+        document.getElementById('error').classList.remove('hidden')
+        return
+    } else {
+        document.getElementById('error').classList.remove('show')
+        document.getElementById('error').classList.add('hidden')
     }
-    document.getElementById('name').innerHTML = `${input.charAt(0).toUpperCase() + input.slice(1)}`;
+    // Update page with pokemon data
+    let pokemon = await response.json()
+    // debugger
+    document.getElementById('update_name').innerHTML = pokemon.name.charAt(0).toUpperCase() + input.slice(1);
+    document.getElementById('update_type').innerHTML = `${pokemon.types[0].type.name} / ${pokemon.types[1].type.name}`;
+    document.getElementById('update_height').innerHTML = `${pokemon.height / 10}m / ${(pokemon.height * 3.937.toFixed(2))}"`;
+    document.getElementById('update_weight').innerHTML = `${pokemon.weight / 10}kg / ${(pokemon.weight / 4.536).toFixed(2)}lbs`;
+    console.log(pokemon.types[0]);
+    
 });
 
-
-// If Pokémon entry doesn't exist, show missingno
-//
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     document.querySelectorAll('img').forEach( function (img) {
-//        img.onerror = function() {this.style.display='none';};
-//     })
-//  });
